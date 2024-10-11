4db0f1fc949253ac8f37d834182e67c567b341ee
Hrishikesh Swain <108139026+hrishi0902@users.noreply.github.com>
2024-10-11 21:21:53 +0530

    missing number

---
+++ Java/array.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int missingNumber(int n, int[] arr)
+    {
+        // Create hash array of size n+1
+        int[] hash = new int[n + 1];
+
+        // Store frequencies of elements
+        for (int i = 0; i < n - 1; i++) {
+            hash[arr[i]]++;
+        }
+
+        // Find the missing number
+        for (int i = 1; i <= n; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+
+        // Edge case handling (though problem guarantees a
+        // solution)
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 3, 5 };
+        int n = 5;
+        System.out.println(missingNumber(n, arr));
+    }
+}
